caa52f30ae3938311813252bec7ec41e1e14eaa2
jmcia2020 <jeanmmcmahon@gmail.com>
2020-12-17 17:13:41 -0600

    update css path

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var userName = prompt('Please enter your first name.');
+alert('Hello ' + userName + ', click ok to go to page.');
+
+    if(userName === 'Craig' || userName === 'Yo'){
+      alert('Hello ' + userName + ', click ok to go to YO YO  page.');
+      // || or && and 
+    } else if (userName === 'Bob' && userName === ' Smith'){
+      alert('Hello ' + userName + ', click BOb BOB ok to go to page.');
+    } else {
+      alert('Hello ' + userName + ', click ok to go to page.');
+    }
+
+    var favoriteFood = prompt('Is Pizza your favorite food.').trim();
+  console.log(favoriteFood);
+    if(favoriteFood === 'N' || favoriteFood === 'No'){
+      alert('Are you sure? Have you even tried it?');
+    } else if (favoriteFood === 'Y' || favoriteFood === 'Yes'){
+      alert('Yes Pizza is my favorite too....');
+    } else {
+      alert('Please go eat pizza.');
+    }
