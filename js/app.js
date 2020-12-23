309d4eecd3097f20fa2257d2ec74034f4338a655
jmcia2020 <jeanmmcmahon@gmail.com>
2020-12-23 03:19:07 -0600

    changes to nav bar and other css changes. Date Update attempt

--- js/app.js
+++ js/app.js
@@ -11,3 +11,27 @@ var deltaTeam = prompt('Hello ' + userName + '! Are you a developer?').trim().to
     } else {
       alert("I don't think you understand Booleans.");
     }
+
+
+    <script language="javascript"> 
+      months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+      var d=new Date();
+      var weekday=new Array(7);
+      weekday[0]="Sunday";
+      weekday[1]="Monday";
+      weekday[2]="Tuesday";
+      weekday[3]="Wednesday";
+      weekday[4]="Thursday";
+      weekday[5]="Friday";
+      weekday[6]="Saturday";
+
+      var d = new Date();
+      (d.getFullYear());
+
+
+      var theDate = new Date(document.lastModified); 
+      theDate.setTime((theDate.getTime()+(60*60)) ) 
+      with (theDate) { 
+      document.write("<i>Last updated "+weekday[getDay()]+' '+getDate()+' '+months[getMonth()]+' '+d.getFullYear()+' '+getHours()+':'+getMinutes()+" GMT</i>") 
+      } 
+      </script> 
